639c4e8e1869a2422a44470b5d038e292492708e
awaluwalakhiru <awalprasetyo4@gmail.com>
2022-11-18 15:04:34 +0700

    first init setup inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from "@inertiajs/inertia-vue3";
+
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, props, app, plugin }) {
+        createApp({
+            render: () => h(app, props),
+        }).use(plugin).mount(el);
+    }
+});
