65510f61adaa11bd0327e3c12185f88b796e6c91
awaluwalakhiru <awalprasetyo4@gmail.com>
2022-11-18 15:26:40 +0700

    first setup inertia progressbar and init new setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,30 @@ require('./bootstrap');
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from '@inertiajs/progress';
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
     setup({ el, props, app, plugin }) {
         createApp({
             render: () => h(app, props),
-        }).use(plugin).mount(el);
+        })      //set mixins
+            .mixin({
+                methods: {
+                    //method "hasAnyPermission"
+                    hasAnyPermission: function (permissions) {
+                        //get permissions from props
+                        let allPermissions = this.$page.props.auth.permissions;
+                        let hasPermission = false;
+                        permissions.forEach(function (item) {
+                            if (allPermissions[item]) hasPermission = true;
+                        });
+                        return hasPermission;
+                    }
+                },
+            })
+            .use(plugin).mount(el);
     }
 });
+
+InertiaProgress.init();
